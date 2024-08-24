e551bb5d712a30b6a8464f2c72a197acd3b740ea
Agnes <agnes.mccall5@gmail.com>
2024-08-24 16:50:10 -0400

    use useEffect to shuffle new deck

--- src/Cards.js
+++ src/Cards.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import axios from "axios";
 
 const Cards = () => {
+    const deckId = useRef();
+
+    useEffect(() => {
+        async function shuffleDeck() {
+            const response = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+            console.log(response.data);
+            deckId.current = response.data.deck_id;
+            console.log(deckId.current);
+        }
+        shuffleDeck();
+    }, []);
+
     return <div>Cards</div>;
 }; 
 
